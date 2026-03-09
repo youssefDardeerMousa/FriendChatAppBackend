3c05d417130f642a9554a09f2b4b685fcde0e5d9
youssef dardeer <youssef.dardeer.mousa@gmail.com>
2026-03-09 15:10:54 +0200

    edit on logout code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -77,13 +77,19 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 
 export const updateProfile = async (req, res) => {
   try {
